9a538ea6dbb8fe16512fdbcf88419046a868c28d
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2006-12-30 18:13:45 +0000

    sidux-kernelhacking (2.6.19.6) unstable; urgency=low
      * fix bashisms in kernel install-/ remove scripts.
      * refactor kernel install script.
    
     -- Stefan Lippers-Hollmann <s.l-h@gmx.de>  Sat, 30 Dec 2006 19:12:37 +0100

--- scripts/install-kernel-sidux.sh
+++ scripts/install-kernel-sidux.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if [ $(id -u) != 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
+	[ -x /usr/bin/su-me ] && DISPLAY="" exec su-me "$0" "$@"
+
 	echo Error: You must be root to run this script!
 	exit 1
 fi
@@ -8,6 +10,7 @@ fi
 VER=%KERNEL_VERSION%
 SUB=1
 ALSA=
+X_CONF="/etc/X11/xorg.conf"
 
 rm -f	/boot/System.map \
 	/boot/vmlinuz \
@@ -15,92 +18,56 @@ rm -f	/boot/System.map \
 
 grep -q '  */sbin/update-grub$' /etc/kernel-img.conf 2> /dev/null && sed -i 's%=.*sbin/update-grub%= update-grub%' /etc/kernel-img.conf 2> /dev/null
 
+# install important dependencies
+[ -x /usr/bin/gcc-4.1 ]      || apt-get install gcc-4.1
+[ -x /usr/sbin/mkinitramfs ] || apt-get install initramfs-tools
+
+# install kernel, headers and our patches to the vanilla tree
 dpkg -i linux-image-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
 dpkg -i linux-headers-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
 dpkg -i linux-doc-"$VER"_"$SUB"_all.deb
 test -n "$ALSA" && dpkg -i alsa-modules-"$VER"_"$ALSA"+"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
 test -f linux-custom-patches-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb && dpkg -i linux-custom-patches-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
 
-#ln -sf System.map-$VER /boot/System.map
-#ln -sf vmlinuz-$VER /boot/vmlinuz
+ln -fs "vmlinuz-$VER"		/boot/vmlinuz
+ln -fs "boot/vmlinuz-$VER"	/vmlinuz
 
-[ -x /usr/sbin/yaird ] || apt-get install yaird
-mkinitrd.yaird -o /boot/initrd.img-$VER $VER
-if [ -e /boot/initrd.img-$VER ]; then
-	#ln -sf initrd.img-$VER /boot/initrd.img
-	update-grub
+# we do need an initrd
+if [ ! -r "/boot/initrd.img-$VER" ]; then
+	mkinitramfs -o "/boot/initrd.img-$VER" "$VER"
 fi
+ln -fs "initrd.img-$VER"	/boot/initrd.img
+ln -fs "boot/initrd.img-$VER"	/initrd.img
 
-# install important dependencies
-[ -x /usr/bin/gcc-4.1 ] || apt-get install gcc-4.1
-dpkg -l module-init-tools &>/dev/null || apt-get -y install module-init-tools
-update-rc.d module-init-tools start 20 S . >/dev/null
+ln -fs "System.map-$VER"	/boot/System.map
 
-rm -rf /usr/src/linux /usr/src/linux-$VER /lib/modules/$VER/build
-if [ ! -d /usr/src/linux-headers-$VER/scripts ]; then
-	rm -f /usr/src/linux-headers-$VER/scripts
-fi
+# in case we just created an initrd, update menu.lst
+update-grub
 
-ln -s linux-headers-$VER /usr/src/linux-$VER
-ln -s /usr/src/linux-$VER /lib/modules/$VER/build  
-cp -f /boot/config-$VER /usr/src/linux-$VER/.config
-rm -rf /usr/src/linux-$VER/Documentation
-ln -s /usr/share/doc/linux-doc-$VER/Documentation /usr/src/linux-$VER/Documentation
-ln -sf boot/vmlinuz-$VER /vmlinuz
+# set symlinks to the kernel headers
+rm -f "/lib/modules/$VER/build" >/dev/null 2>&1
+ln -s "/usr/src/linux-headers-$VER" "/lib/modules/$VER/build"
+ln -fs "linux-headers-$VER" /usr/src/linux >/dev/null 2>&1
 
 # remove agpgart, fglrx, radeon modules
-perl -pi -e 's/^agpgart\n?//' /etc/modules
-perl -pi -e 's/^fglrx\n?//' /etc/modules
-perl -pi -e 's/^radeon\n?//' /etc/modules
-
-# hack for new installer
-X_CONF=XF86Config-4
-if which Xorg >/dev/null; then
-	[ -e /etc/X11/xorg.conf ] && X_CONF=xorg.conf
-fi
+perl -pi -e 's/^agpgart\n?//'	/etc/modules
+perl -pi -e 's/^fglrx\n?//'	/etc/modules
+perl -pi -e 's/^radeon\n?//'	/etc/modules
 
 # hints for fglrx
-if grep -q '"fglrx"' "/etc/X11/$X_CONF"; then
+if grep -q '"fglrx"' "$X_CONF"; then
 	echo "ATI RADEON 3D acceleraction will NOT work with the new kernel until"
 	echo "the driver is reinstalled."
 	echo
 fi
 
 # workaround for nvidia
-if grep -q '"nvidia"' /etc/X11/$X_CONF; then
-	perl -pi -e 's/^([\s]*Driver\s*)"nvidia"/\1"nv"/g' "/etc/X11/$X_CONF"
+if grep -q '"nvidia"' "$X_CONF"; then
+	perl -pi -e 's/^([\s]*Driver\s*)"nvidia"/\1"nv"/g' "$X_CONF"
 	grep -q ^nvidia /etc/modules || echo nvidia >> /etc/modules
 	echo "NVIDIA driver has been DISABLED!"
 	echo
 fi
-
-# eepro100 fix
-sed -i s/eepro100/e100/ /etc/modules
-
-# psmouse fix
-grep -q ^psmouse /etc/modules || echo psmouse >> /etc/modules
-
-# fix modules
-rm -f /etc/modules-*
-
-# mouse fix
-perl -pi -e 's|(\s*Option\s+"Protocol"\s+)"auto"|\1"IMPS/2|' "/etc/X11/$X_CONF"
-[ -f "/etc/X11/$X_CONF.1st" ] && perl -pi -e 's|(\s*Option\s+"Protocol"\s+)"auto"|\1"IMPS/2"|' "/etc/X11/$X_CONF.1st"
-echo 'Notice: the mouse protocol "auto" has been changed to "IMPS/2"!'
-echo 'If you have problems change it to "PS/2" - "auto" does not work with 2.6.'
-echo "Change was done in /etc/X11/$X_CONF (and /etc/X11/$X_CONF.1st)."
-echo
-
-# change usbdevfs to usbfs with lowered right setting
-perl -pi -e "s|.*/proc/bus/usb.*|usbfs  /proc/bus/usb  usbfs  devmode=0666  0  0|" /etc/fstab
-echo usbdevfs has been replaced by usbfs in /etc/fstab with devmode=0666
-
-# camera group hack
-USER=$(grep 1000 /etc/passwd|cut -f1 -d:)
-GROUP=$(echo $(groups $USER|cut -f2 -d:)|sed "s/ /,/g")
-echo $GROUP|grep -q camera || (
-[ "$USER" ] &&  usermod -G $GROUP,camera $USER
-)
 
 # alsa sound hack
 rm -f /var/lib/alsa/asound.state
