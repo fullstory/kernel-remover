e5696cb252a0f87aef819fad3c5580e3cd6a2a22
Michiel Lieuwe de Boer <ltown2@web.de>
2007-01-20 16:12:50 +0000

    Untested change: duplicate UUID detection in fstab2uuid.pl and general reliability improvements

--- fstab2uuid.pl
+++ fstab2uuid.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
-$ARGV[0] = "/etc/fstab";
+$self = "Fstab2uuid";
+$ARGV[0] = "/etc/fstab.test";
 $^I=".sidux-orig";
-while(<>) { if (m|^(/dev/[hs]d[a-z][1-9][0-9]?)\s|) { chomp($uuid = `/lib/udev/vol_id -u $1`); 	$uuid and s/$1/UUID=$uuid/ } print }
+while(<>) { 
+	if (m|^(/dev/[hs]d[a-z][1-9][0-9]?)\s|) { 
+		chomp($uuid = `/lib/udev/vol_id -u $1`);
+		if ($uuid and ! $?) {
+			if ($h{$uuid}) {
+				print STDERR "${self} error: duplicate UUID '$uuid' found!\n";
+			}
+			else {
+				$h{$uuid}++;
+				s/$1/UUID=$uuid/;
+			}
+		}
+	} 
+	print;
+}
 
