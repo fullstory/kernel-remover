04c8ec2d20fdc70af767798f66eb79ec7838344f
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-07-10 01:51:03 +0000

    * fix thinko.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -303,7 +303,7 @@ dpkg_patches() {
 
 	printf "${COLOR_ACTION}Preserving custom patches in debian archive${COLOR_NORM}...\n"
 	printf "%-70s [" "  * ${COLOR_INFO}linux-custom-patches-${KERNEL}${COLOR_NORM}"
-	if [[ ! -d /usr/share/sidux-kernelhacking/linux-custom-patches ]] || [[ ! -x $(which fakeroot) ]] || [[ ! -x $(which dpkg-dev) ]]; then
+	if [[ ! -d /usr/share/sidux-kernelhacking/linux-custom-patches ]] || [[ ! -x $(which fakeroot) ]] || [[ ! -x $(which /usr/bin/dpkg-buildpackage) ]]; then
 		printf "${COLOR_FAILURE}Skipped!${COLOR_NORM}]\n"
 		printf "    ${COLOR_FAILURE}Ensure to have dpkg-dev, fakeroot and sidux-kernelhacking installed.${COLOR_NORM}\n"
 		return 0
