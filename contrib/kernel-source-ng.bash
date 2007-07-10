30636534c299a4602ea6c5f2d4c7fe5f1f3cf9cf
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-07-10 23:34:49 +0000

    * actually add 2.6.22_cfg80211_netdev_security.diff.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -94,6 +94,7 @@ patches_for_kernel() {
 			PATCH+=( $MIRROR/people/akpm/patches/2.6/2.6.22-rc6/2.6.22-rc6-mm1/broken-out/gregkh-usb-usb-oti6858-status.patch )
 			PATCH+=( $PATCH_MIRROR/2.6.22-squashfs3.2-r2.patch.bz2 )
 			PATCH+=( $PATCH_MIRROR/2.6.22.1_aacraid_security.patch )
+			PATCH+=( $PATCH_MIRROR/2.6.22_cfg80211_netdev_security.diff )
 			;;
 		*)
 			#PATCH+=(  )
