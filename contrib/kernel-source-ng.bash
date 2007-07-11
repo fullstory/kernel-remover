251bbe32d4096f3deeb23860cb9fe88f30a247e1
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-07-11 00:15:26 +0000

    * fix probing for dpkg-buildpackage.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -308,7 +308,7 @@ dpkg_patches() {
 	printf "%-70s [" "  * ${COLOR_INFO}linux-custom-patches-${KERNEL}${COLOR_NORM}"
 	if [[ ! -d /usr/share/sidux-kernelhacking/linux-custom-patches ]] || \
 	   [[ ! -x $(which fakeroot) ]] || \
-	   [[ ! -x $(which /usr/bin/dpkg-buildpackage) ]] || \
+	   [[ ! -x $(which dpkg-buildpackage) ]] || \
 	   [[ ! -r /usr/share/cdbs/1/rules/debhelper.mk ]]; then
 		printf "${COLOR_FAILURE}Skipped!${COLOR_NORM}]\n"
 		printf "    ${COLOR_FAILURE}Ensure to have the following packages installed:${COLOR_NORM}\n"
