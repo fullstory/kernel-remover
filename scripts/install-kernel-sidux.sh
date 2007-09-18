78abbd86a4f043df16063c6b96d767dc7513442a
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-09-18 08:41:48 +0000

    * overhaul install-kernel scripts. * remove obsolete cruft:   - remove-kernel-sidux.sh, kernel-remover does a better job.

--- scripts/install-kernel-sidux.sh
+++ scripts/install-kernel-sidux.sh
@@ -10,7 +10,7 @@ fi
 VER=%KERNEL_VERSION%
 SUB=1
 ALSA=
-X_CONF="/etc/X11/xorg.conf"
+ARCH="$(dpkg-architecture -qDEB_BUILD_ARCH)"
 
 rm -f	/boot/System.map \
 	/boot/vmlinuz \
@@ -18,63 +18,65 @@ rm -f	/boot/System.map \
 	/etc/initramfs-tools/conf.d/resume
 
 # fix /etc/kernel-img.conf
-sed -i	-e s/postinst_hook.*/postinst_hook\ \=\ \\/usr\\/sbin\\/update-grub/ \
-	-e s/postrm_hook.*/postrm_hook\ \=\ \\/usr\\/sbin\\/update-grub/ \
-	-e s/do_initrd.*/do_initrd\ \=\ Yes/ \
-	-e /ramdisk.*mkinitrd\\.yaird/d \
+sed -i	-e "s/\(postinst_hook\).*/\1\ \=\ \\/usr\\/sbin\\/update-grub/" \
+	-e "s/\(postrm_hook\).*/\1\ \=\ \\/usr\\/sbin\\/update-grub/" \
+	-e "s/\(do_initrd\).*/\1\ \=\ Yes/" \
+	-e "/ramdisk.*mkinitrd\\.yaird/d" \
 		/etc/kernel-img.conf
+
 grep -q do_initrd /etc/kernel-img.conf 2> /dev/null || \
 	echo "do_initrd = Yes" >> /etc/kernel-img.conf
 
 
 # install important dependencies
-[ -x /usr/bin/gcc-4.1 ]		|| apt-get install gcc-4.1
+[ -x /usr/bin/gcc-4.2 ]		|| apt-get install gcc-4.2
 [ -x /usr/sbin/mkinitramfs ]	|| apt-get install initramfs-tools
 
 # install kernel, headers and our patches to the vanilla tree
-dpkg -i linux-image-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
-dpkg -i linux-headers-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
-dpkg -i linux-doc-"$VER"_"$SUB"_all.deb
-test -n "$ALSA" && dpkg -i alsa-modules-"$VER"_"$ALSA"+"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
-test -f linux-custom-patches-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb && dpkg -i linux-custom-patches-"$VER"_"$SUB"_$(dpkg-architecture -qDEB_BUILD_ARCH).deb
+dpkg -i "linux-image-${VER}_${SUB}_${ARCH}.deb"
+dpkg -i "linux-headers-${VER}_${SUB}_${ARCH}.deb"
+dpkg -i "linux-doc-${VER}_${SUB}_all.deb"
+[ -n "$ALSA" ] && dpkg -i "alsa-modules-${VER}_${ALSA}+${SUB}_${ARCH}.deb"
+[ -f "linux-custom-patches-${VER}_${SUB}_${ARCH}.deb" ] && dpkg -i "linux-custom-patches-${VER}_${SUB}_${ARCH}.deb"
 
-ln -fs "vmlinuz-$VER"		/boot/vmlinuz
-ln -fs "boot/vmlinuz-$VER"	/vmlinuz
+ln -fs "vmlinuz-${VER}"		/boot/vmlinuz
+ln -fs "boot/vmlinuz-${VER}"	/vmlinuz
 
 # we do need an initrd
-if [ ! -r "/boot/initrd.img-$VER" ]; then
-	mkinitramfs -o "/boot/initrd.img-$VER" "$VER"
+if [ ! -r "/boot/initrd.img-${VER}" ]; then
+	mkinitramfs -o "/boot/initrd.img-${VER}" "${VER}"
 fi
-ln -fs "initrd.img-$VER"	/boot/initrd.img
-ln -fs "boot/initrd.img-$VER"	/initrd.img
 
-ln -fs "System.map-$VER"	/boot/System.map
+# set new kernel as default
+ln -fs "initrd.img-${VER}"	/boot/initrd.img
+ln -fs "boot/initrd.img-${VER}"	/initrd.img
+ln -fs "System.map-${VER}"	/boot/System.map
 
 # in case we just created an initrd, update menu.lst
 update-grub
 
 # set symlinks to the kernel headers
-rm -f "/lib/modules/$VER/build" >/dev/null 2>&1
-ln -s "/usr/src/linux-headers-$VER" "/lib/modules/$VER/build"
-ln -fs "linux-headers-$VER" /usr/src/linux >/dev/null 2>&1
+rm -f "/lib/modules/${VER}/build" >/dev/null 2>&1
+ln -s "/usr/src/linux-headers-${VER}" "/lib/modules/${VER}/build"
+ln -fs "linux-headers-${VER}" /usr/src/linux >/dev/null 2>&1
 
 # remove agpgart, fglrx, radeon modules
-perl -pi -e 's/^agpgart\n?//'	/etc/modules
-perl -pi -e 's/^fglrx\n?//'	/etc/modules
-perl -pi -e 's/^radeon\n?//'	/etc/modules
+sed -i	-e "/^[\ \t]\?agpgart/d" \
+	-e "/^[\ \t]\?fglrx/d" \
+	-e "/^[\ \t]\?radeon/d" \
+		/etc/modules
 
 # hints for fglrx
-if grep -q '"fglrx"' "$X_CONF"; then
-	echo "ATI RADEON 3D acceleraction will NOT work with the new kernel until"
-	echo "the driver is reinstalled."
+if grep -q '"fglrx"' /etc/X11/xorg.conf; then
+	echo "ATI RADEON 3D acceleration will NOT work with the new kernel until"
+	echo "the non-free fglrx driver is reinstalled."
 	echo
 fi
 
 # workaround for nvidia
-if grep -q '"nvidia"' "$X_CONF"; then
-	perl -pi -e 's/^([\s]*Driver\s*)"nvidia"/\1"nv"/g' "$X_CONF"
-	grep -q ^nvidia /etc/modules || echo nvidia >> /etc/modules
-	echo "NVIDIA driver has been DISABLED!"
+if grep -q '"nvidia"' /etc/X11/xorg.conf; then
+	sed -i "s/^\([\ \t]\?Driver.*\)\"nvidia\"/\1\"nv\"/g" /etc/X11/xorg.conf
+	echo "non-free nVidia driver has been DISABLED!"
 	echo
 fi
 
