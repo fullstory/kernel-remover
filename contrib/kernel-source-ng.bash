210f2e487421ec174ae7725759ea82309040cda7
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-09-26 20:00:35 +0000

    * update unionfs to 2.1.5.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -52,7 +52,7 @@ patches_for_kernel() {
 			PATCH+=(
 				http://gaugusch.at/acpi-dsdt-initrd-patches/acpi-dsdt-initrd-v0.8.4-2.6.21.patch
 				$PATCH_MIRROR/t-sinus_111card-2.6.16.diff
-				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.4_for_2.6.22.6.diff.gz
+				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.5_for_2.6.22.6.diff.gz
 				$PATCH_MIRROR/2.6.22.2-at76_usb20070810.diff.bz2
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-fat-utf8.patch.bz2
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-unicode-vt.patch.bz2
@@ -78,7 +78,7 @@ patches_for_kernel() {
 			PATCH+=(
 				http://gaugusch.at/acpi-dsdt-initrd-patches/acpi-dsdt-initrd-v0.8.4-2.6.21.patch
 				$PATCH_MIRROR/t-sinus_111card-2.6.16.diff
-				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.4_for_2.6.23-rc7.diff.gz
+				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.5_for_2.6.23-rc8.diff.gz
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-fat-utf8.patch.bz2
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-unicode-vt.patch.bz2
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm1/broken-out/git-ipwireless_cs.patch
