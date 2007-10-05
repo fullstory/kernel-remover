2c02990a3e6d16c2e6cd8582eb1d332a83fac9c3
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-10-05 10:24:32 +0000

    * fix conflicts with current -git.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -84,7 +84,6 @@ patches_for_kernel() {
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/git-ipwireless_cs.patch
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/gregkh-driver-nozomi.patch
 				$PATCH_MIRROR/2.6.23-rc6_ipg-add-ip1000a-driver-to-kernel-tree.diff.bz2
-				$PATCH_MIRROR/2.6.23-rc9-git2_wireless.patch.bz2
 				$PATCH_MIRROR/2.6.23-rc9-git2_wireless-pending.patch.bz2
 				$PATCH_MIRROR/2.6.23-rc9-git2_at76.patch.bz2
 				$PATCH_MIRROR/2.6.23-rc9-git2_ath5k.patch.bz2
