fba934f20fa11a809580a4bc38080591dfffd3c6
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-11-04 14:44:19 +0000

    * remove highres timers, which have proven itself to be problematic on some   i686 systems. * update wireless to 2.6.23.1-42.wl.2. * update rt2x00 to 2.0.12+gitv2.6.23-rc3-8647-g130dd8f. * add experimental support for RealTek RTL8185 wlan cards.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -50,7 +50,6 @@ patches_for_kernel() {
 	case "$1" in
 		2.6.23*)
 			PATCH+=(
-				$PATCH_MIRROR/2.6.23.1_highres-timers-hrt3.patch.bz2
 				http://gaugusch.at/acpi-dsdt-initrd-patches/acpi-dsdt-initrd-v0.8.4-2.6.21.patch
 				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.8_for_2.6.23.1.diff.gz
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-fat-utf8.patch.bz2
@@ -58,15 +57,16 @@ patches_for_kernel() {
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/git-ipwireless_cs.patch
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/gregkh-driver-nozomi.patch
 				$PATCH_MIRROR/2.6.23-rc6_ipg-add-ip1000a-driver-to-kernel-tree.diff.bz2
-				$PATCH_MIRROR/2.6.23.1-35.wl.2_wireless.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-35.wl.2_wireless-pending.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-35.wl.2_at76.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-35.wl.2_ath5k.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-35.wl.2_zd1211rw-mac80211.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-35.wl.2_mac80211-decryption-noise.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-35.wl.2_rt2x00-2.0.12.diff.bz2
-				$PATCH_MIRROR/rt2x00_show-info.diff
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_wireless.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_wireless-pending.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_at76.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_ath5k.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_ath5k-fixes.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_zd1211rw-mac80211.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_mac80211-decryption-noise.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-42.wl.2_rt2x00-2.0.12+git-8647-g130dd8f.diff.bz2
 				$PATCH_MIRROR/rt2x00_no-macbuf.diff
+				$PATCH_MIRROR/2.6.23.1_add-rtl8180-wireless-driver.diff.bz2
 				$PATCH_MIRROR/2.6.23.1_netdev-e1000e-01.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_netdev-e1000e-02.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_netdev-e1000e-03.patch.bz2
