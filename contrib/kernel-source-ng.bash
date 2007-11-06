9f72f69f584794d06a29f2dbf8b558b0233008f1
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-11-06 14:50:23 +0000

    * radeon: set the address to access the GART table on the CPU side   correctly.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -89,6 +89,7 @@ patches_for_kernel() {
 				$PATCH_MIRROR/2.6.23.1_lirc-0.8.2-current.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_alsa-1.0.15-merge-1_1.1.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_alsa-1.0.15-merge-2_1.1.patch.bz2
+				$PATCH_MIRROR/2.6.23.1_drm-radeon-x800-pcie-fix_1.1.patch
 			)
 			;;
 		2.6.24*)
