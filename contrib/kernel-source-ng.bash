cfb596cf8d4bc2c0d486a7ed11d148f9e2b994e0
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-11-15 15:42:56 +0000

    * update wireless to 2.6.23.1-53.wl.1. * import 2.6.23.2-rc1. * import 2.6.23.3-rc1. * import 2.6.23.4-rc1. * import 2.6.23.5-rc1. * import 2.6.23.6-rc1. * import 2.6.23.7-rc1. * add several ALSA quriks for hda and friends. * kill stupid input messages. * add additional ALPS IDs. * add additional Wacom IDs. * update rt2x00 to 2.0.12+git-8649-g39e0501.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -50,6 +50,15 @@ patches_for_kernel() {
 	case "$1" in
 		2.6.23*)
 			PATCH+=(
+				http://www.kernel.org/pub/linux/kernel/v2.6/stable-review/patch-2.6.23.2-rc1.gz
+				http://www.kernel.org/pub/linux/kernel/v2.6/stable-review/patch-2.6.23.3-rc1.gz
+				http://www.kernel.org/pub/linux/kernel/v2.6/stable-review/patch-2.6.23.4-rc1.gz
+				$PATCH_MIRROR/2.6.23.4-rc1_revert-conflicts-with-wireless-2.6.diff
+				http://www.kernel.org/pub/linux/kernel/v2.6/stable-review/patch-2.6.23.5-rc1.gz
+				$PATCH_MIRROR/2.6.23.5-rc1_revert-conflicts-with-wireless-2.6.diff
+				http://www.kernel.org/pub/linux/kernel/v2.6/stable-review/patch-2.6.23.6-rc1.gz
+				$PATCH_MIRROR/2.6.23.6-rc1_alsa-1.0.15.diff
+				http://www.kernel.org/pub/linux/kernel/v2.6/stable-review/patch-2.6.23.7-rc1.gz
 				http://gaugusch.at/acpi-dsdt-initrd-patches/acpi-dsdt-initrd-v0.8.4-2.6.21.patch
 				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.9_for_2.6.23.1.diff.gz
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-fat-utf8.patch.bz2
@@ -57,12 +66,13 @@ patches_for_kernel() {
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/git-ipwireless_cs.patch
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/gregkh-driver-nozomi.patch
 				$PATCH_MIRROR/2.6.23-rc6_ipg-add-ip1000a-driver-to-kernel-tree.diff.bz2
-				$PATCH_MIRROR/2.6.23.1-49.fc8_wireless.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-49.fc8_wireless-pending.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-49.fc8_at76.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-49.fc8_ath5k.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-49.fc8_zd1211rw-mac80211.patch.bz2
-				$PATCH_MIRROR/2.6.23.1-49.fc8.2_rt2x00-2.0.12+cvs20071111.diff.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_wireless.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_wireless-pending.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_rt2x00-revert-cts-no-ack.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_at76.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_ath5k.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_zd1211rw-mac80211.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_rt2x00-2.0.12+git-8649-g39e0501.diff.bz2
 				$PATCH_MIRROR/rt2x00_no-macbuf.diff
 				$PATCH_MIRROR/2.6.23.1_add-rtl8180-wireless-driver.diff.bz2
 				$PATCH_MIRROR/2.6.23.1_netdev-e1000e-01.patch.bz2
@@ -80,14 +90,21 @@ patches_for_kernel() {
 				$PATCH_MIRROR/2.6.23.1_e1000e_fix-error-checks.diff.bz2
 				$PATCH_MIRROR/2.6.23.1_e1000e_fix-debug-printk-macro.diff.bz2
 				$PATCH_MIRROR/2.6.23.1_e100-disable-polling.patch
-				$PATCH_MIRROR/2.6.23.1_i915-fix-vbl-swap-allocation-size.patch
 				$PATCH_MIRROR/2.6.23.1_esp-scsi.diff.bz2
 				$PATCH_MIRROR/2.6.23.1_thinkpad-key-events.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_usb-storage-initialize-huawei-e220-properly.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_lirc-0.8.2-current.patch.bz2
-				$PATCH_MIRROR/2.6.23.1_alsa-1.0.15-merge-1_1.1.patch.bz2
-				$PATCH_MIRROR/2.6.23.1_alsa-1.0.15-merge-2_1.1.patch.bz2
-				$PATCH_MIRROR/2.6.23.1_drm-radeon-x800-pcie-fix_1.1.patch
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_alsa-1.0.15-merge-1.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_alsa-1.0.15-merge-2.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_alsa.git-000-771af442.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_alsa.git-004-ba76a374.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_alsa-revert-hda-stac-volume.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_alsa-hda-stac-dmic.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_input-kill-stupid-messages.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_input-alps-add-dell-vostro-1400.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_input-alps-add-thinkpad-r61.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_serial-460800.patch.bz2
+				$PATCH_MIRROR/2.6.23.1-53.wl.1_serial_pnp-add-new-wacom-ids.patch.bz2
 			)
 			;;
 		2.6.24*)
