2acc549cce8e8dfb4849b4386d01db56f50a488f
Kel Modderman <kel@otaku42.de>
2007-12-04 06:12:43 +0000

    * Give scripts/install-kernel-sidux.sh some love:   - don't remove /etc/initramfs-tools/conf.d/resume   - create minimal /etc/kernel-img.conf if missing   - add extra checks for the dep of udev-config-sidux   - use a recent /usr/sbin/get-resume-partition to fix resume conf   - detect modules that are externally supplied for current local use, if     they are currently used add them to the list to be installed   - after invoking dpkg -i on the list to be installed, check return value     and run apt-get -f install if not equal to zero   - use update-initramfs and not mkinitramfs   - remove fglrx/radeon/agpgart hack, i think that was never employed in     sidux release, ever   - add a hint for people using madwifi drivers   - do not remove /var/lib/alsa/asound.state   - cleanup final message, hint the manual for more information on UUID and     LABEL usage

--- scripts/install-kernel-sidux.sh
+++ scripts/install-kernel-sidux.sh
@@ -9,37 +9,58 @@ fi
 
 VER=%KERNEL_VERSION%
 SUB=1
-ALSA=
 ARCH="$(dpkg-architecture -qDEB_BUILD_ARCH)"
 
 rm -f	/boot/System.map \
 	/boot/vmlinuz \
-	/boot/initrd.img \
-	/etc/initramfs-tools/conf.d/resume
+	/boot/initrd.img
 
-# fix /etc/kernel-img.conf
-sed -i	-e "s/\(postinst_hook\).*/\1\ \=\ \\/usr\\/sbin\\/update-grub/" \
-	-e "s/\(postrm_hook\).*/\1\ \=\ \\/usr\\/sbin\\/update-grub/" \
-	-e "s/\(do_initrd\).*/\1\ \=\ Yes/" \
-	-e "/ramdisk.*mkinitrd\\.yaird/d" \
-		/etc/kernel-img.conf
+# ensure /etc/kernel-img.conf is configured with correct settings
+# - fix path to update-grub
+# - make sure "do_initrd = Yes"
+if [ -w /etc/kernel-img.conf ]; then
+	echo -n "Checking /etc/kernel-img.conf..."
 
-grep -q do_initrd /etc/kernel-img.conf 2> /dev/null || \
-	echo "do_initrd = Yes" >> /etc/kernel-img.conf
+	sed -i	-e "s/\(postinst_hook\).*/\1\ \=\ \\/usr\\/sbin\\/update-grub/" \
+		-e "s/\(postrm_hook\).*/\1\ \=\ \\/usr\\/sbin\\/update-grub/" \
+		-e "s/\(do_initrd\).*/\1\ \=\ Yes/" \
+		-e "/ramdisk.*mkinitrd\\.yaird/d" \
+			/etc/kernel-img.conf
+	
+	if ! grep -q do_initrd /etc/kernel-img.conf; then
+		echo "do_initrd = Yes" >> /etc/kernel-img.conf
+	fi
 
+	echo "done."
+else
+	echo "WARNING: /etc/kernel-img.conf is missing, creating file with defaults..."
+	cat > /etc/kernel-img.conf <<EOF
+do_bootloader = No
+postinst_hook = /usr/sbin/update-grub
+postrm_hook   = /usr/sbin/update-grub
+do_initrd     = Yes
+EOF
+fi
 
-# install important dependencies
-[ -x /usr/bin/gcc-4.2 ]		|| INSTALL_DEP="$INSTALL_DEP gcc-4.2"
-[ -x /usr/sbin/mkinitramfs ]	|| INSTALL_DEP="$INSTALL_DEP initramfs-tools"
+# install important dependencies before attempting to install kernel
+unset INSTALL_DEP
+[ -x /usr/bin/gcc-4.2 ]           || INSTALL_DEP="$INSTALL_DEP gcc-4.2"
+[ -x /usr/sbin/update-initramfs ] || INSTALL_DEP="$INSTALL_DEP initramfs-tools"
 
 # take care to install b43-fwcutter, if bcm43xx-fwcutter is already installed
-if dpkg -l bcm43xx-fwcutter >/dev/null 2>&1 || test -r /lib/firmware/bcm43xx_pcm4.fw; then
+if dpkg -l bcm43xx-fwcutter 2>/dev/null | grep -q '^[hi]i' || [ -e /lib/firmware/bcm43xx_pcm4.fw ]; then
 	INSTALL_DEP="$INSTALL_DEP b43-fwcutter"
 fi
 
-# do not blacklist b43, we need it for kernel >= 2.6.23
-# make sure to install the IEEE1394 vs. FireWire "Juju" blacklist
-if [ -r /etc/modprobe.d/sidux ] && dpkg --compare-versions $(dpkg -l udev-config-sidux >/dev/null 2>&1 | awk '/^ii/{print $3}') lt 0.4.3 >/dev/null 2>&1; then
+# make sure udev-config-sidux is up to date
+# - do not blacklist b43, we need it for kernel >= 2.6.23
+# - make sure to install the IEEE1394 vs. FireWire "Juju" blacklist
+if [ -r /etc/modprobe.d/sidux ] || [ -r /etc/modprobe.d/ieee1394 ] || [ -r /etc/modprobe.d/mac80211 ]; then
+	dpkg --compare-versions $(dpkg -l udev-config-sidux >/dev/null 2>&1 | awk '/^[hi]i/{print $3}') lt 0.4.3 >/dev/null 2>&1
+	if [ "$?" -eq 0 ]; then
+		INSTALL_DEP="$INSTALL_DEP udev-config-sidux"
+	fi
+else
 	INSTALL_DEP="$INSTALL_DEP udev-config-sidux"
 fi
 
@@ -48,19 +69,51 @@ if [ -n "$INSTALL_DEP" ]; then
 	apt-get install $INSTALL_DEP
 fi
 
-# install kernel, headers and our patches to the vanilla tree
-dpkg -i "linux-image-${VER}_${SUB}_${ARCH}.deb"
-dpkg -i "linux-headers-${VER}_${SUB}_${ARCH}.deb"
-dpkg -i "linux-doc-${VER}_${SUB}_all.deb"
-[ -n "$ALSA" ] && dpkg -i "alsa-modules-${VER}_${ALSA}+${SUB}_${ARCH}.deb"
-[ -f "linux-custom-patches-${VER}_${SUB}_${ARCH}.deb" ] && dpkg -i "linux-custom-patches-${VER}_${SUB}_${ARCH}.deb"
+# check resume partition configuration is valid
+if [ -x /usr/sbin/get-resume-partition ]; then
+	dpkg --compare-versions $(dpkg -l sidux-scripts >/dev/null 2>&1 | awk '/^[hi]i/{print $3}') ge 0.1.38 >/dev/null 2>&1
+	if [ "$?" -eq 0 ]; then
+		get-resume-partition
+	fi
+fi
+
+# externally supplied modules, check to see if currently in use
+unset EXTRA_DEBS
+for deb in *-${VER}_*+${SUB}_${ARCH}.deb; do
+	[ -f "$deb" ] || continue
+	
+	for mod in $(dpkg --contents $deb | sed -n -e 's%-%_%g;s%.*/\([^\.]\+\)\.ko$%\1%p'); do
+		[ -n "$mod" ] || continue
+		
+		if [ -d "/sys/module/$mod" ] || grep -w -q "^$mod" /proc/modules; then
+			EXTRA_DEBS="$EXTRA_DEBS $deb"
+			break
+		fi
+	done
+done
+
+# our patches to the vanilla tree
+if [ -f "linux-custom-patches-${VER}_${SUB}_${ARCH}.deb" ]; then
+	EXTRA_DEBS="$EXTRA_DEBS linux-custom-patches-${VER}_${SUB}_${ARCH}.deb"
+fi
+
+# install kernel, headers, documentation and any extras that were detected
+dpkg -i "linux-image-${VER}_${SUB}_${ARCH}.deb" \
+	"linux-headers-${VER}_${SUB}_${ARCH}.deb" \
+	"linux-doc-${VER}_${SUB}_all.deb" \
+	$EXTRA_DEBS
+
+# something went wrong, allow apt an attempt to fix it
+if [ "$?" -ne 0 ]; then
+	apt-get --fix-broken install
+fi
 
 ln -fs "vmlinuz-${VER}"		/boot/vmlinuz
 ln -fs "boot/vmlinuz-${VER}"	/vmlinuz
 
 # we do need an initrd
-if [ ! -r "/boot/initrd.img-${VER}" ]; then
-	mkinitramfs -o "/boot/initrd.img-${VER}" "${VER}"
+if [ ! -f "/boot/initrd.img-${VER}" ]; then
+	update-initramfs -k "${VER}" -c
 fi
 
 # set new kernel as default
@@ -69,44 +122,50 @@ ln -fs "boot/initrd.img-${VER}"	/initrd.img
 ln -fs "System.map-${VER}"	/boot/System.map
 
 # in case we just created an initrd, update menu.lst
-update-grub
+if [ -x /usr/sbin/update-grub ]; then
+	update-grub
+fi
 
 # set symlinks to the kernel headers
 rm -f "/lib/modules/${VER}/build" >/dev/null 2>&1
 ln -s "/usr/src/linux-headers-${VER}" "/lib/modules/${VER}/build"
 ln -fs "linux-headers-${VER}" /usr/src/linux >/dev/null 2>&1
 
-# remove agpgart, fglrx, radeon modules
-sed -i	-e "/^[\ \t]\?agpgart/d" \
-	-e "/^[\ \t]\?fglrx/d" \
-	-e "/^[\ \t]\?radeon/d" \
-		/etc/modules
-
 # hints for fglrx
 if grep -q '"fglrx"' /etc/X11/xorg.conf; then
+	echo
 	echo "ATI RADEON 3D acceleration will NOT work with the new kernel until"
 	echo "the non-free fglrx driver is reinstalled."
 	echo
 fi
 
-# workaround for nvidia
+# hints for nvidia
 if grep -q '"nvidia"' /etc/X11/xorg.conf; then
 	sed -i "s/^\([\ \t]\?Driver.*\)\"nvidia\"/\1\"nv\"/g" /etc/X11/xorg.conf
-	echo "non-free nVidia driver has been DISABLED!"
+	echo
+	echo "nVidia 3D acceleration will NOT work with the new kernel until"
+	echo "the non-free nVidia driver is reinstalled."
 	echo
 fi
 
-# alsa sound hack
-rm -f /var/lib/alsa/asound.state
-echo alsa sound will be muted next start.
-echo use "alsactl store" as root to save it after checking the volumes.
+# hints for madwifi
+if [ -d /sys/module/ath_pci ] || grep -q '^ath_pci' /proc/modules; then
+	echo
+	echo "Atheros Wireless Network Adaptor will not work until"
+	echo "the non-free madwifi driver is reinstalled."
+	echo
+fi
 
 # grub notice
-echo 'Now you can simply reboot when using GRUB (default). In case you use'
-echo 'LILO you have to do the mentioned changes manually.'
-echo ""
-echo "Make sure that /etc/fstab and /boot/grub/menu.lst use UUID- or LABEL-"
+echo
+echo "Now you can simply reboot when using GRUB (default). If you use the LILO"
+echo "bootloader you will have to configure it to use the new kernel."
+echo 
+echo "Make sure that /etc/fstab and /boot/grub/menu.lst use UUID or LABEL"
 echo "based mounting, which is required for classic IDE vs. lib(p)ata changes!"
-
-echo Have fun!
-
+echo
+echo "For more details about UUID or LABEL fstab usage see the sidux manual:"
+echo "   http://manual.sidux.com/en/part-cfdisk-en.htm#disknames"
+echo
+echo "Have fun!"
+echo
