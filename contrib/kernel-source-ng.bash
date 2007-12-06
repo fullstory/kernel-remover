dd017c44c29444e09f43b0ed91a363ecfa4c999f
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-12-06 10:54:14 +0000

    * some b43 related debugging.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -62,16 +62,17 @@ patches_for_kernel() {
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/gregkh-driver-nozomi.patch
 				$PATCH_MIRROR/2.6.23.8_ipg-add-ip1000a-driver.diff.bz2
 				$PATCH_MIRROR/2.6.23.9-77.fc8_wireless.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_wireless-pending.patch.bz2
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_wireless-pending.patch.bz2
 				$PATCH_MIRROR/2.6.23.9-77.fc8_at76.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_ath5k.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_zd1211rw-mac80211.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_rtl8180.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_b43-rev-d.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_ath5k-use-soft-wep.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_cfg80211-extras.patch.bz2
-				$PATCH_MIRROR/2.6.23.9-77.fc8_rt2x00-2.0.13.diff.bz2
-				$PATCH_MIRROR/rt2x00_no-macbuf.diff
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_ath5k.patch.bz2
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_zd1211rw-mac80211.patch.bz2
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_rtl8180.patch.bz2
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_b43-rev-d.patch.bz2
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_ath5k-use-soft-wep.patch.bz2
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_cfg80211-extras.patch.bz2
+				#$PATCH_MIRROR/2.6.23.9-77.fc8_rt2x00-2.0.13.diff.bz2
+				#$PATCH_MIRROR/rt2x00_no-macbuf.diff
+				$PATCH_MIRROR/2.6.23.9-77.fc8-2.6.24_rt2x00-2.0.13.diff.bz2
 				$PATCH_MIRROR/2.6.23.1_netdev-e1000e-01.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_netdev-e1000e-02.patch.bz2
 				$PATCH_MIRROR/2.6.23.1_netdev-e1000e-03.patch.bz2
