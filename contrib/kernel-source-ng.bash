7324ea0a3a9127fa45688c403ddf6a78876abe8e
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2008-01-01 11:55:50 +0000

    * update unionfs to 2.2. * update nozomi to 2.6.24-rc6-mm1. * update ipwireless_cs to 2.6.24-rc6-mm1.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -56,7 +56,7 @@ patches_for_kernel() {
 				$PATCH_MIRROR/2.6.23.9-rc1_revert-conflicts-with-wireless-2.6.diff
 				$PATCH_MIRROR/2.6.23.10-rc1_revert-conflicts-with-wireless-2.6.diff
 				http://gaugusch.at/acpi-dsdt-initrd-patches/acpi-dsdt-initrd-v0.8.4-2.6.21.patch
-				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.11_for_2.6.23.9.diff.gz
+				$PATCH_MIRROR/unionfs-2.x/unionfs-2.2_for_2.6.23.12.diff.gz
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-fat-utf8.patch.bz2
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-unicode-vt.patch.bz2
 				$MIRROR/people/akpm/patches/2.6/2.6.23-rc8/2.6.23-rc8-mm2/broken-out/git-ipwireless_cs.patch
@@ -113,10 +113,10 @@ patches_for_kernel() {
 		2.6.24*)
 			PATCH+=(
 				http://gaugusch.at/acpi-dsdt-initrd-patches/acpi-dsdt-initrd-v0.8.4-2.6.21.patch
-				$PATCH_MIRROR/unionfs-2.1/unionfs-2.1.11_for_2.6.24-rc4.diff.gz
+				$PATCH_MIRROR/unionfs-2.x/unionfs-2.2_for_2.6.24-rc6.diff.gz
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-fat-utf8.patch.bz2
-				$MIRROR/people/akpm/patches/2.6/2.6.24-rc4/2.6.24-rc4-mm1/broken-out/git-ipwireless_cs.patch
-				$MIRROR/people/akpm/patches/2.6/2.6.24-rc4/2.6.24-rc4-mm1/broken-out/gregkh-driver-nozomi.patch
+				$MIRROR/people/akpm/patches/2.6/2.6.24-rc6/2.6.24-rc6-mm1/broken-out/git-ipwireless_cs.patch
+				$MIRROR/people/akpm/patches/2.6/2.6.24-rc6/2.6.24-rc6-mm1/broken-out/gregkh-driver-nozomi.patch
 				$PATCH_MIRROR/2.6.23.9-80.fc8_at76.patch.bz2				# almost independent from mac80211/ wireless-2.6
 				$PATCH_MIRROR/2.6.24-rc4-git5_rt2x00-2.0.13.diff.bz2
 				$PATCH_MIRROR/2.6.24-rc4-git5_lirc-0.8.2-current.patch.bz2
