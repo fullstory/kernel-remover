37fcdd000a19bbfcb0803f520d475b80a1e34db9
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2008-01-07 00:32:34 +0000

    * add experimental support for RealTek RTL8180L and RTL8185 wlan cards.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -119,6 +119,7 @@ patches_for_kernel() {
 				$MIRROR/people/akpm/patches/2.6/2.6.24-rc6/2.6.24-rc6-mm1/broken-out/gregkh-driver-nozomi.patch
 				$PATCH_MIRROR/2.6.23.9-80.fc8_at76.patch.bz2				# almost independent from mac80211/ wireless-2.6
 				#$PATCH_MIRROR/2.6.24-rc7_rt2x00-2.0.14.diff.bz2
+				$PATCH_MIRROR/2.6.24-rc7_rtl8180l-rtl8185_20080106.diff.bz2
 				$PATCH_MIRROR/2.6.24-rc4-git5_lirc-0.8.2-current.patch.bz2
 				$PATCH_MIRROR/sec_perm-2.6.24-gpl.patch
 				$PATCH_MIRROR/2.6.24-rc6_make_sysfs_optional.patch
