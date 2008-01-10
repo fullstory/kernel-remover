346b9e01d8e7edc4beb8e2bfe305d39f5dd99a38
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2008-01-10 17:54:43 +0000

    * revert put-802.11-data-on-4-byte-boundary.diff again.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -120,7 +120,6 @@ patches_for_kernel() {
 				$MIRROR/people/akpm/patches/2.6/2.6.24-rc6/2.6.24-rc6-mm1/broken-out/gregkh-driver-nozomi.patch
 				$PATCH_MIRROR/2.6.23.9-80.fc8_at76.patch.bz2				# almost independent from mac80211/ wireless-2.6
 				$PATCH_MIRROR/rt2x00_correctly-initialize-rt2500usb-mac.diff		# fix rt2500usb support
-				$PATCH_MIRROR/rt2x00_put-802.11-data-on-4-byte-boundary.diff
 				$PATCH_MIRROR/2.6.24-rc7_rtl8180l-rtl8185_20080106.diff.bz2
 				$PATCH_MIRROR/2.6.24-rc4-git5_lirc-0.8.2-current.patch.bz2
 				$PATCH_MIRROR/sec_perm-2.6.24-gpl.patch
