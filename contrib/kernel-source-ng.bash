dabcedc23f7e494305ca1af43934ae7d55492108
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2008-01-10 17:47:28 +0000

    * fix unionfs path for 2.6.24.

--- contrib/kernel-source-ng.bash
+++ contrib/kernel-source-ng.bash
@@ -114,7 +114,7 @@ patches_for_kernel() {
 		2.6.24*)
 			PATCH+=(
 				http://gaugusch.at/acpi-dsdt-initrd-patches/acpi-dsdt-initrd-v0.8.4-2.6.21.patch
-				$PATCH_MIRROR/unionfs-2.x/unionfs-2.x/unionfs-2.2.2_for_2.6.24-rc7.diff.gz
+				$PATCH_MIRROR/unionfs-2.x/unionfs-2.2.2_for_2.6.24-rc7.diff.gz
 				$PATCH_MIRROR/2.6.22-1.3242_defaults-fat-utf8.patch.bz2
 				$MIRROR/people/akpm/patches/2.6/2.6.24-rc6/2.6.24-rc6-mm1/broken-out/git-ipwireless_cs.patch
 				$MIRROR/people/akpm/patches/2.6/2.6.24-rc6/2.6.24-rc6-mm1/broken-out/gregkh-driver-nozomi.patch
